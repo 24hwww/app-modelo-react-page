084a5827f9d0e0f7b32368aaa53984cae90fa9b4
BOOK-SPF4J9F7VP\leona <24hwww@gmail.com>
2024-08-26 21:19:47 -0300

    prueba 2

--- my-react-app/src/pages/home.jsx
+++ my-react-app/src/pages/home.jsx
@@ -1,5 +1,8 @@
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton  } from '@ionic/react';
-import { Link } from "react-router-dom";
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonGrid, IonRow, IonCol  } from '@ionic/react';
+
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonChip  } from '@ionic/react';
+
+//import { Link } from "react-router-dom";
 
 export default function Home({title}) {
   return (
@@ -14,7 +17,51 @@ export default function Home({title}) {
 
         </IonToolbar>
         </IonHeader>
-        <IonContent>Main View Content<Link to="/">back to app</Link></IonContent>
+        <IonContent>
+        
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                
+              <IonCard>
+                <IonCardHeader>
+                  <IonCardSubtitle><IonChip>Pedidos</IonChip></IonCardSubtitle>
+                  <IonCardTitle>$ 125.0251.99</IonCardTitle>
+                  
+                </IonCardHeader>
+
+                <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+              </IonCard>
+
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol>
+                <IonCard>
+                  <IonCardHeader>
+                    <IonCardSubtitle><IonChip>Productos</IonChip></IonCardSubtitle>
+                    <IonCardTitle>1.365</IonCardTitle>
+                  </IonCardHeader>
+
+                  <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+                </IonCard>                
+              </IonCol>
+              <IonCol>
+              <IonCard>
+                  <IonCardHeader>
+                    <IonCardSubtitle><IonChip>Clientes</IonChip></IonCardSubtitle>
+                    <IonCardTitle>234</IonCardTitle>
+                  </IonCardHeader>
+
+                  <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+                </IonCard>      
+              </IonCol>
+            </IonRow>
+
+          </IonGrid>       
+        
+        </IonContent>
         </>
   );
 }
